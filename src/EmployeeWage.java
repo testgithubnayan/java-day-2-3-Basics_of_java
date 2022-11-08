1d6b697f25068760525e08a90b4c0e21c3c1466a
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-08 14:51:38 +0530

    calculating part time employee wage .................

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -5,39 +5,23 @@ public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME__HOUR = 4;
-    static final int IS_FULL_TIME = 2;
-    static final int IS_PART_TIME = 1;
-    static final int WORKING_DAY_PER_MONTH = 20;
-    static final int WORKING_HOUR_PER_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation");
         int empPresent = (int) (Math.floor(Math.random()*10) % 3);
-        int dayCount = 1;
-        int workingHours = 0;
-        int totalWage = 0;
-        while (dayCount <= WORKING_DAY_PER_MONTH && workingHours <= WORKING_HOUR_PER_MONTH) {
-            int empWage = 0;
-            switch (empPresent) {
-                case IS_FULL_TIME:
-                    empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    workingHours += FULL_DAY_HOUR;
-                    System.out.println("Employee FULL-TIME");
-                    break;
-                case IS_PART_TIME:
-                    empWage = WAGE_PER_HOUR * PART_TIME__HOUR;
-                    workingHours += PART_TIME__HOUR;
-                    System.out.println("Employee PART-TIME");
-                    break;
-
-                default:
-                    System.out.println("Employee is Absent");
-            }
-            totalWage += empWage;
-            System.out.println("Employee daily wage Day : " +dayCount + " => " + empWage);
-            dayCount++;
+        int empWage = 0;
+        if(empPresent == 2){
+            empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("Employee FULL-TIME");
         }
-        System.out.println("working Hours: " + workingHours);
-        System.out.println("Total wage: " + totalWage);
+        else if(empPresent == 1){
+            empWage = WAGE_PER_HOUR * PART_TIME__HOUR;
+            System.out.println("Employee PART-TIME");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+
+        System.out.println("Employee daily wage: " + empWage);
     }
 }
